951b6195a227d4f6d30a3b919457891a5eed793e
Kevin Tourret <tarpion@toto.fr>
2021-10-12 16:54:49 +0200

    Correction Yatzee

--- yatzee/index.ts
+++ yatzee/index.ts
@@ -1,8 +1,37 @@
 
 class Dice {
 
+    private _value: number;
+
+    constructor() {
+        this._value = Math.floor(Math.random() * (6 - 1)) + 1;
+    }
+
+    get value(): number {
+        return this._value;
+    }
 }
 
 class Yatzee {
 
+    private _myDice: Array<Dice>;
+
+    throwDice(): void {
+        // C'EST LA MEME CHOSE
+        // this._myDice = new Array<Dice>();
+        this._myDice = [];
+        for (let i = 1; i <= 5; i++) {
+            // new Dice => instancie un objet Dice
+            // Dans le constructeur de Dice, on génère sa valeur, du coup, on a des dés avec déjà une valeur
+            this._myDice.push(new Dice());
+        }
+    }
+
+    get myDice(): Array<Dice> {
+        return this._myDice;
+    }
 }
+
+const yatzee = new Yatzee();
+yatzee.throwDice();
+console.log(yatzee.myDice);
